6863a3ae4536f7afb6301660063caf93c9327126
Your Name <you@example.com>
2024-03-20 15:08:00 +0000

    UPDATE
    Changes to file locations
    Added node_modules
    generateMarkDown.js is complete
    index.js is complete

--- application/utils/generateMarkdown.js
+++ application/utils/generateMarkdown.js
@@ -1,18 +1,29 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const profileImg = `https://github.com/${data.username}.png?size=50`;
+  return `
+# ${data.title}
+## Description
+${data.description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [Licence](#license)
+* [Tests](#tests)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## Credits
+${data.credits}
+## License 
+${data.licence}
+## Tests
+${data.tests}
+## Questions
+
+If you have any further questions about my repo, you can open an issue or contact me directly at ${data.email}.
+You can also find more of my previous work at [${data.github}](https://github.com/${data.github}/).
 
 `;
 }
